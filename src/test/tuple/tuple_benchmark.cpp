07ea7ea0d7eb33fbe92719456189f925848d82f8
Daniel Frey <d.frey@gmx.de>
2018-06-27 19:21:55 +0200

    Formatting

--- src/test/tuple/tuple_benchmark.cpp
+++ src/test/tuple/tuple_benchmark.cpp
@@ -3,39 +3,30 @@
 
 #include <tao/tuple/tuple.hpp>
 
-#include <type_traits>
 #include <tuple>
+#include <type_traits>
 
 // To benchmark the standard library's version, enable this:
 // #define tao std
 
 int main()
 {
-  auto t = tao::tuple_cat( tao::make_tuple( 1, true ),
-			   tao::make_tuple( 1.0 ),
-			   tao::make_tuple(),
-			   tao::make_tuple( 1UL, nullptr ) );
+   auto t = tao::tuple_cat( tao::make_tuple( 1, true ),
+                            tao::make_tuple( 1.0 ),
+                            tao::make_tuple(),
+                            tao::make_tuple( 1UL, nullptr ) );
 
-  static_assert( std::is_same< decltype( t ), tao::tuple< int, bool, double, unsigned long, std::nullptr_t > >::value, "oops" );
+   static_assert( std::is_same< decltype( t ), tao::tuple< int, bool, double, unsigned long, std::nullptr_t > >::value, "oops" );
 
-  auto t2 = tao::tuple_cat( t, tao::make_tuple( 1, true ),
-        		    t, tao::make_tuple( 1.0 ),
-        		    t, tao::make_tuple(),
-        		    t, tao::make_tuple( 1UL, nullptr ) );
+   auto t2 = tao::tuple_cat( t, tao::make_tuple( 1, true ), t, tao::make_tuple( 1.0 ), t, tao::make_tuple(), t, tao::make_tuple( 1UL, nullptr ) );
 
-  static_assert( tao::tuple_size< decltype( t2 ) >::value == 25, "oops" );
+   static_assert( tao::tuple_size< decltype( t2 ) >::value == 25, "oops" );
 
-  auto t3 = tao::tuple_cat( t2, tao::make_tuple( 1, true ),
-                            t2, tao::make_tuple( 1.0 ),
-                            t2, tao::make_tuple(),
-                            t2, tao::make_tuple( 1UL, nullptr ) );
+   auto t3 = tao::tuple_cat( t2, tao::make_tuple( 1, true ), t2, tao::make_tuple( 1.0 ), t2, tao::make_tuple(), t2, tao::make_tuple( 1UL, nullptr ) );
 
-  static_assert( tao::tuple_size< decltype( t3 ) >::value == 105, "oops" );
+   static_assert( tao::tuple_size< decltype( t3 ) >::value == 105, "oops" );
 
-  auto t4 = tao::tuple_cat( t3, tao::make_tuple( 1, true ),
-                            t2, tao::make_tuple( 1.0 ),
-                            t3, tao::make_tuple(),
-                            t2, tao::make_tuple( 1UL, nullptr ) );
+   auto t4 = tao::tuple_cat( t3, tao::make_tuple( 1, true ), t2, tao::make_tuple( 1.0 ), t3, tao::make_tuple(), t2, tao::make_tuple( 1UL, nullptr ) );
 
-  static_assert( tao::tuple_size< decltype( t4 ) >::value == 265, "oops" );
+   static_assert( tao::tuple_size< decltype( t4 ) >::value == 265, "oops" );
 }
--- src/test/tuple/tuple_cat.cpp
+++ src/test/tuple/tuple_cat.cpp
@@ -3,30 +3,30 @@
 
 #include <tao/tuple/tuple.hpp>
 
-#include <type_traits>
 #include <cassert>
+#include <type_traits>
 
 int main()
 {
-  using namespace tao;
+   using namespace tao;
 
-  auto t = tuple_cat( make_tuple( 1, true ),
-                      make_tuple( 2.0 ),
-                      make_tuple(),
-                      make_tuple( 3UL, nullptr ) );
+   auto t = tuple_cat( make_tuple( 1, true ),
+                       make_tuple( 2.0 ),
+                       make_tuple(),
+                       make_tuple( 3UL, nullptr ) );
 
-  static_assert( tuple_size< decltype( t ) >::value == 5, "oops" );
-  static_assert( std::is_same< decltype( t ), tuple< int, bool, double, unsigned long, std::nullptr_t > >::value, "oops" );
+   static_assert( tuple_size< decltype( t ) >::value == 5, "oops" );
+   static_assert( std::is_same< decltype( t ), tuple< int, bool, double, unsigned long, std::nullptr_t > >::value, "oops" );
 
-  assert( get< 0 >( t ) == 1 );
-  assert( get< 1 >( t ) == true );
-  assert( get< 2 >( t ) == 2.0 );
-  assert( get< 3 >( t ) == 3UL );
-  assert( get< 4 >( t ) == nullptr );
+   assert( get< 0 >( t ) == 1 );
+   assert( get< 1 >( t ) == true );
+   assert( get< 2 >( t ) == 2.0 );
+   assert( get< 3 >( t ) == 3UL );
+   assert( get< 4 >( t ) == nullptr );
 
-  assert( get< int >( t ) == 1 );
-  assert( get< bool >( t ) == true );
-  assert( get< double >( t ) == 2.0 );
-  assert( get< unsigned long >( t ) == 3 );
-  assert( get< std::nullptr_t >( t ) == nullptr );
+   assert( get< int >( t ) == 1 );
+   assert( get< bool >( t ) == true );
+   assert( get< double >( t ) == 2.0 );
+   assert( get< unsigned long >( t ) == 3 );
+   assert( get< std::nullptr_t >( t ) == nullptr );
 }
