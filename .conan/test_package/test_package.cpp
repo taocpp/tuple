4383f4e2f7b74796237fd9e3e6b75bf735b153ac
Daniel Frey <d.frey@gmx.de>
2019-03-13 07:10:28 +0100

    Add clang-7, clang-8 and scan-build to CI

--- .conan/test_package/test_package.cpp
+++ .conan/test_package/test_package.cpp
@@ -1,5 +1,5 @@
 // The Art of C++ / Tuple
-// Copyright (c) 2015-2018 Daniel Frey
+// Copyright (c) 2015-2019 Daniel Frey
 // Please see LICENSE for license or visit https://github.com/taocpp/operators/
 
 #include <tao/tuple/tuple.hpp>
